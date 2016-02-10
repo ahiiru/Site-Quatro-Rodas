2e8d7c82424a68c35e43d9e3dc7dbecf3ceaee1e
ahiiru <contato@felipemota.com>
2016-02-10 15:49:43 -0200

    Adição do site responsivo

---
+++ index.php
@@ -0,0 +1,263 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon.ico?v=1">
+
+    <title>Quatro Rodas</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/navbar-fixed-top.css" rel="stylesheet">
+    
+    <!-- Yet more custom styles -->
+    <link href="css/custom.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <!-- Menu superior fixo -->
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="f-container-nav">
+        <div class="container">
+          <a href="#"><img src="images/ad.jpg" class="img-responsive center-block f-ad-top" alt=""></a>
+        </div>
+        
+        <div class="container">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span class="sr-only">Navegação</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">
+              <h1>Quatro Rodas</h1>
+              <img src="images/logo.png">
+            </a>
+          </div>
+          <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav navbar-right">
+              <li><a href="#">Carros <span class="glyphicon glyphicon-chevron-down" aria-hidden="true"></span></a></li>
+              <li><a href="#">Testes <span class="glyphicon glyphicon-chevron-down"></span></a></li>
+              <li><a href="#">Notícias <span class="glyphicon glyphicon-chevron-down"></span></a></li>
+              <li><a href="#">Auto-Serviço <span class="glyphicon glyphicon-chevron-down"></span></a></li>
+              <li><a href="#">Guia de Compras <span class="glyphicon glyphicon-chevron-down"></span></a></li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Tabela FIPE <span class="glyphicon glyphicon-chevron-down"></span></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Action</a></li>
+                  <li><a href="#">Another action</a></li>
+                  <li><a href="#">Something else here</a></li>
+                  <li role="separator" class="divider"></li>
+                  <li class="dropdown-header">Nav header</li>
+                  <li><a href="#">Separated link</a></li>
+                  <li><a href="#">One more separated link</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Assine</a></li>
+              <div class="input-group input-group-sm f-search">
+                <input type="text" class="form-control" placeholder="Pesquisar" aria-describedby="sizing-addon3">
+                <span class="input-group-btn">
+                  <button class="btn btn-default" type="button"><span class="glyphicon glyphicon-search"></span></button>
+                </span>
+              </div>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div>
+      </div>
+    </nav>
+
+    <div class="container">
+
+      <!-- Segunda barra superior -->
+      <div class="f-secondbar">
+        <p>
+          <a href="#" class="f-secondbar-a f-secondbar-a-first">+Acessados</a>
+          <a href="#" class="f-secondbar-a">Salão do Automóvel</a>
+          <a href="#" class="f-secondbar-a">Renegade</a>
+          <a href="#" class="f-secondbar-a">Novo Sandero</a>
+          <a href="#" class="f-secondbar-a">Novo Fox</a>
+          <a href="#" class="f-secondbar-a">Novo Ka</a>
+          <a href="#" class="f-secondbar-a">HB 20</a>
+          <a href="#" class="f-secondbar-a">Duster</a>
+          <a href="#" class="f-secondbar-a">Golf</a>
+          <a href="#" class="f-secondbar-a">Corolla</a>
+          <a href="#" class="f-secondbar-a">Civic</a>
+          <a href="#" class="f-secondbar-a">|A-Z|</a>
+        </p>
+      </div>
+
+    </div> <!-- /container -->
+
+    <div class="container">
+
+      <!-- Notícias em destaque -->
+      <div class="container-fluid f-featured">
+        <div class="row">
+        
+          <!-- Principal -->
+          <a href="#">
+            <div class="col-xs-12 col-sm-12 col-md-6 f-cover f-featured-img f-featured-img-bmw">
+              <div class="f-featured-text">
+                <h2><span class="glyphicon glyphicon-stop" aria-hidden="true"></span>Carros grandes</h2>
+                <h3>BMW confirma novos X1<br>
+                e série 3 no Brasil</h3>
+              </div>
+            </div>
+          </a>
+          
+          <a href="#">
+            <div class="col-xs-12 col-sm-6 col-md-3 f-cover f-featured-img f-featured-img-kia">
+              <div class="f-featured-text">
+                <h2><span class="glyphicon glyphicon-stop" aria-hidden="true"></span>Fabricantes</h2>
+                <h3 class="small">Frankfurt:<br>
+                Kia revela novo Sportage</h3>
+              </div>
+            </div>
+          </a>
+          
+          <!-- Publicidade -->
+          <div class="col-xs-12 col-sm-6 col-md-3 f-cover f-featured-img f-featured-img-ad">
+            <a href="#"><span>Publicidade</span><img src="images/ad2.jpg" class="img-responsive" alt=""></a>
+          </div>
+          
+          <a href="#">
+            <div class="col-xs-12 col-sm-6 col-md-3 f-cover f-featured-img f-featured-img-porsche">
+              <div class="f-featured-text">
+                <h2><span class="glyphicon glyphicon-stop" aria-hidden="true"></span>Fabricantes</h2>
+                <h3 class="small">Frankfurt: Porsche Mission e<br>
+                dá um show...</h3>
+              </div>
+            </div>
+          </a>
+          
+          <a href="#">
+            <div class="col-xs-12 col-sm-6 col-md-3 f-cover f-featured-img f-featured-img-vw">
+              <div class="f-featured-text">
+                <h2><span class="glyphicon glyphicon-stop" aria-hidden="true"></span>Promovido por: Volkswagen</h2>
+                <h3 class="small">Nova geração<br>
+                do Volkswagen Tiguan</h3>
+              </div>
+            </div>
+          </a>
+          
+        </div>
+      </div>
+
+    </div> <!-- /container -->
+    
+    <div class="container">
+
+      <!-- Notícias recentes -->
+      <div class="container-fluid f-recent-columns f-recent-sides">
+        <div class="row">
+        
+          <div class="col-xs-12 col-sm-6 col-md-3">
+            <a href="#">
+              <div class="row">
+                <div class="f-cover f-recent-img f-recent-img-audi">
+                  <div class="f-recent-text">
+                    <h2><span class="glyphicon glyphicon-stop" aria-hidden="true"></span>Comparativos</h2>
+                  </div>
+                </div>
+              </div>
+            </a>
+            <div class="row">
+              <div class="f-recent-desc">
+                <h3>Audi S3 x Mercedes-Benz CLA 45 AMG</h3>
+                <p>Pista livre para o embate entre os dois cometas de cauda curta</p>
+                <p class="f-recent-x"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span>Troller T4 X Jeep Renegade</p>
+              </div>
+            </div>
+          </div>
+          
+          <div class="col-xs-12 col-sm-6 col-md-3">
+            <a href="#">
+              <div class="row">
+                <div class="f-cover f-recent-img f-recent-img-nissan">
+                  <div class="f-recent-text">
+                    <h2><span class="glyphicon glyphicon-stop" aria-hidden="true"></span>Impressões</h2>
+                  </div>
+                </div>
+              </div>
+            </a>
+            <div class="row">
+              <div class="f-recent-desc">
+                <h3>Nissan GT-R</h3>
+                <p>Pilotamos o Godzilla, o monstro recordista de Nürburgring que assombra os alemães</p>
+                <p class="f-recent-x"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span>Troller T4 X Jeep Renegade</p>
+              </div>
+            </div>
+          </div>
+          
+          <div class="col-xs-12 col-sm-6 col-md-3">
+            <a href="#">
+              <div class="row">
+                <div class="f-cover f-recent-img f-recent-img-bmw">
+                  <div class="f-recent-text">
+                    <h2><span class="glyphicon glyphicon-stop" aria-hidden="true"></span>Impressões</h2>
+                  </div>
+                </div>
+              </div>
+            </a>
+            <div class="row">
+              <div class="f-recent-desc">
+                <h3>BMW série 2 Active Tourer</h3>
+                <p>Pista livre para o embate entre os dois cometas de cauda curta</p>
+                <p class="f-recent-x"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span>Troller T4 X Jeep Renegade</p>
+              </div>
+            </div>
+          </div>
+          
+          <div class="col-xs-12 col-sm-6 col-md-3">
+            <a href="#">
+              <div class="row">
+                <div class="f-cover f-recent-img f-recent-img-chevrolet">
+                  <div class="f-recent-text">
+                    <h2><span class="glyphicon glyphicon-stop" aria-hidden="true"></span>Auto-Serviço</h2>
+                  </div>
+                </div>
+              </div>
+            </a>
+            <div class="row">
+              <div class="f-recent-desc">
+                <h3>Chevrolet mostra nova geração do Malibu</h3>
+                <p>Sedã perdeu 136 kg e ampliou o espaço entre-eixos</p>
+                <p class="f-recent-x"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span>Troller T4 X Jeep Renegade</p>
+              </div>
+            </div>
+          </div>
+          
+        </div> <!-- /row -->
+      </div>
+
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/jquery.min.js"><\/script>')</script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
